fd464025a9a44496f834fcd08b3ffcb46dbf38f0
Herberth Gomez <gomezgaray04@gmail.com>
2017-11-12 11:32:28 -0600

    Added mongoose for database connection
    - Added mongoose
    - Running site when successfully connected to db

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 Computer-Shop
 
-Requirements
+
+**Requirements**
 - MongoDb
 - NodeJs
 
+
 ## Running ComputerShop
 
+- Before to run computershop application make sure your mongo db is running
+- Then type the following command in the project root folder:
 `npm start`
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Module dependencies
-var express = require('express');
+var express  = require('express'),
+    mongoose = require('mongoose');
 
 // Server creation
 var app = express();
@@ -12,7 +13,15 @@ app.get('/', function(request, response){
     response.send('Welcome!!');
 });
 
-// Starting server
-app.listen(port, function(){
-    console.log("Running in port " + port);
+// Creating connection with database
+var mongoDb = mongoose.connect('mongodb://localhost/computershop', { useMongoClient: true, }, function (dberror){
+    // Checking if error ocurred connecting to db
+    if( dberror ){
+        console.log('Could not establish a database connection: ' + dberror);
+    } else {
+        // Starting server if conencted to database
+        app.listen(port, function(){
+            console.log("Running in port " + port);
+        });
+    }
 });
