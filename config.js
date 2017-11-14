06061013aa6f2fde8f55dc263ceef7e420ea6669
Herberth Gomez <gomezgaray04@gmail.com>
2017-11-14 17:08:03 +0000

    Fixing connection to mongodb heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'database' : 'mongodb://localhost:27017/computershop',
+    'database' : 'mongodb://heroku_mzfzr3gk:8em8ck4m3cbt0l6nj7is4ksjlp@ds159235.mlab.com:59235/heroku_mzfzr3gk',
     'csToken' : 'computershopherberthgomez'
 };
